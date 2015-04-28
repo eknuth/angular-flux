7b302c560435309c442a82a31764a8a42e51fc89
Edwin Knuth <eknuth@gmail.com>
2015-04-27 20:32:18 -0700

    Actions

--- src/js/dispatchers/app-dispatcher.js
+++ src/js/dispatchers/app-dispatcher.js
@@ -1,7 +1,7 @@
 var Dispatcher = require('./dispatcher.js');
-var merge  = require('react/lib/merge');
+var angular = require('angular');
 
-var AppDispatcher = merge(Dispatcher.prototype, {
+var AppDispatcher = angular.extend({}, Dispatcher.prototype, {
   handleViewAction: function(action){
     this.dispatch({
       source: 'VIEW_ACTION',
