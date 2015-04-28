7a2ab005cb4ad7ac3e9d97c791f07b6a8e79eb8e
Edwin Knuth <eknuth@gmail.com>
2015-04-27 20:32:18 -0700

    Actions

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,10 +1,14 @@
+var AppActions = require('../actions/app-actions.js');
+
 module.exports = function(){
   return {
     restrict: 'E',
-    template: '<h1>MY ANGULAR APP</h1>',
+    template: '<h1 ng-click="handleClick()">MY ANGULAR APP2</h1>',
     replace: true,
     link: function($scope, elm, attrs) {
-
+      $scope.handleClick = function(){
+        AppActions.addItem('this is the item');
+      };
     }
   };
 };
